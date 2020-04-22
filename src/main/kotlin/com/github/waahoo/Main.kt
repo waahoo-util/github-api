daef4533db5b9d7314063464ec5848b9931ad1c1
waahoo <waahoo@outlook.com>
2020-04-23 07:50:16 +0800

    update

--- src/main/kotlin/com/github/waahoo/Main.kt
+++ src/main/kotlin/com/github/waahoo/Main.kt
@@ -5,7 +5,7 @@ import com.github.waahoo.http.initClient
 
 fun main(args: Array<String>) {
   initClient()
-  assert(args.size >= 5)
-  GitHub.downloadAsset(args[0], args[1], args[2], args[3], args[4])
+  assert(args.size >= 4)
+  GitHub.downloadAsset(args[0], args[1], args[2], args[3])
   closeClient()
 }
